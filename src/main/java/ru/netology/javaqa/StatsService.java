a1d8fa30db401ab106eb067217dbd0fcd66027d7
leandreane <monk-86@inbox.ru>
2023-06-16 11:35:10 +0300

    first commit

--- src/main/java/ru/netology/javaqa/StatsService.java
+++ src/main/java/ru/netology/javaqa/StatsService.java
@@ -2,25 +2,25 @@ package ru.netology.javaqa;
 
 public class StatsService {
     public int AmountSalesMin(int[] sales) {
+
         int amountSales = 0;
 
         for (int i = 0; i < sales.length; i++) {
             if (sales[i] <= sales[amountSales]) {
                 amountSales = i;
             }
-
         }
         return amountSales + 1;
     }
 
     public int AmountSalesMax(int[] sales) {
+
         int amountSales = 0;
 
         for (int i = 0; i < sales.length; i++) {
             if (sales[i] >= sales[amountSales]) {
                 amountSales = i;
             }
-
         }
         return amountSales + 1;
     }
@@ -39,6 +39,7 @@ public class StatsService {
     public int AverageAmountSales(int[] sales) {
 
         int amountSales = 0;
+
         for (int i = 0; i < sales.length; i++) {
 
             amountSales += sales[i];
@@ -50,7 +51,9 @@ public class StatsService {
     public int minAverageSales(int[] sales) {
 
         int minAverageMonth = 0;
+
         int averageSum = minAverageSales(sales);
+
         for (long sale : sales) {
             if (sale <= averageSum) {
                 minAverageMonth++;
@@ -63,7 +66,9 @@ public class StatsService {
     public int maxAverageSales(int[] sales) {
 
         int maxAverageMonth = 0;
+
         int averageSum = maxAverageSales(sales);
+
         for (long sale : sales) {
             if (sale >= averageSum) {
                 maxAverageMonth++;
